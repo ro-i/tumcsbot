febc4bd6ebd10e3236b2cabd3e4913649e52ec27
Robert Imschweiler <ro-i@posteo.de>
2020-12-06 23:25:26 +0100

    plugin search: fix typo

--- src/tumcsbot/commands/search.py
+++ src/tumcsbot/commands/search.py
@@ -14,7 +14,7 @@ import tumcsbot.lib as lib
 
 
 class Command(lib.Command):
-    name: str = 'syntax'
+    name: str = 'search'
     syntax: str = 'search <string>'
     description: str = ('get a url to a search for "string" in all public streams')
     msg_template: str = 'Hi, I hope that these search results may help you: {}'
