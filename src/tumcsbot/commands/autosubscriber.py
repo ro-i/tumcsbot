4e5f4518fbfe270b8fdbba2c5018125f5b2315ac
Robert Imschweiler <ro-i@posteo.de>
2021-03-07 10:59:58 +0100

    plugin autosubscriber: use the event api

--- src/tumcsbot/commands/autosubscriber.py
+++ src/tumcsbot/commands/autosubscriber.py
@@ -5,9 +5,10 @@
 
 """Keep the bot subscribed to all public streams.
 
+Reason:
 As the 'all_public_streams' parameter of the event API [1] does not
 seem to work properly, we need a work-around in order to be able to
-receive eventy for all public streams.
+receive events for all public streams.
 
 [1] https://zulip.com/api/register-queue#parameter-all_public_streams
 """
@@ -23,9 +24,7 @@ from tumcsbot.client import Client
 
 class Command(command.CommandDaemon):
     name: str = 'autosubscriber'
-    events: List[str] = []
-    # interval in seconds to run periodically
-    interval: int = 1800
+    events: List[str] = ['stream']
 
     def __init__(self, zuliprc: str, **kwargs: Any) -> None:
         super().__init__(zuliprc)
@@ -37,18 +36,6 @@ class Command(command.CommandDaemon):
         except Exception as e:
             self.logger.exception(e)
 
-    def wait_for_event(self) -> None:
-        """Override CommandDaemon.wait_for_event.
-
-        Do not wait for any event, but for timer to expire.
-        """
-        while True:
-            try:
-                self.event_callback({})
-            except Exception as e:
-                self.logger.exception(e)
-            sleep(type(self).interval)
-
     def func (
         self,
         client: Client,
@@ -58,27 +45,18 @@ class Command(command.CommandDaemon):
         """Do the actual subscribing."""
         result: Dict[str, Any]
 
-        result = client.get_streams(
-            include_public = True,
-            include_web_public = True,
-            include_subscribed = False,
-            include_default = True
-        )
-        if result['result'] != 'success':
-            self.logger.warning(
-                'AutoSubscriber.run(): Cannot get list of all streams: '
-                + str(result)
-            )
+        # Check whether the event indicates a new stream.
+        if event['op'] != 'create':
+            return lib.Response.none()
 
-        streams: List[Dict[str, Any]] = [
-            { 'name': stream['name'] } for stream in result['streams']
-        ]
-
-        result = client.add_subscriptions(streams = streams)
-        if result['result'] != 'success':
-            self.logger.warning(
-                'AutoSubscriber.run(): Cannot subscribe to some streams: '
-                + str(result)
-            )
+        for stream in event['streams']:
+            # Do not autosubscribe to private streams.
+            if stream['invite_only']:
+                continue
+            result = client.add_subscriptions(streams = [{'name': stream['name']}])
+            if result['result'] != 'success':
+                self.logger.warning(
+                    'AutoSubscriber.run(): Cannot subscribe to stream: ' + str(result)
+                )
 
         return lib.Response.none()
