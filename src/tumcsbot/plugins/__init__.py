5af3c9fe53b14be5e445644c36a319c2dd5d43eb
Robert Imschweiler <ro-i@posteo.de>
2021-10-09 12:03:33 +0200

    plugins: Fix mypy issue in __init__.py.

--- src/tumcsbot/plugins/__init__.py
+++ src/tumcsbot/plugins/__init__.py
@@ -1,6 +1,9 @@
 from importlib import import_module as _import_module
 from pkgutil import iter_modules as _iter_modules
 from os.path import dirname as _dirname
+from typing import Any
+
+_: Any
 
 # Import all modules to the current namespace.
 for _, _module_name, _ in _iter_modules([_dirname(__file__)]):
