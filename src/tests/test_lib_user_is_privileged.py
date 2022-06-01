8bc1776499dfd795546cbc63605a02ceda104540
Robert Imschweiler <ro-i@posteo.de>
2022-06-01 21:47:38 +0200

    Fix privilege tests.

--- src/tests/test_lib_user_is_privileged.py
+++ src/tests/test_lib_user_is_privileged.py
@@ -11,7 +11,7 @@ from typing import Any, Dict, List
 from tumcsbot.client import Client
 
 
-@patch.object(Client, '__init__', lambda self: None)
+@patch.object(Client, '__init__', lambda _: None)
 class UserPrivilegedTest(unittest.TestCase):
     def test_invalid_user_data(self) -> None:
         ret: Dict[str, Any] = {'result': 'error'}
@@ -22,7 +22,7 @@ class UserPrivilegedTest(unittest.TestCase):
     def test_no_privilege(self) -> None:
         data: List[Dict[str, Any]] = [
             {'role': -200}, {'role': 0}, {'role': 300}, {'role': 400}, {'role': 600},
-            {'is_admin': False}, {'is_admin': "False"}, {'is_admin': "True"}
+            {'is_admin': False}, {'is_admin': True}, {'is_admin': "False"}, {'is_admin': "True"}
         ]
         for d in data:
             ret: Dict[str, Any] = {'result': 'success', 'user': d}
@@ -32,10 +32,20 @@ class UserPrivilegedTest(unittest.TestCase):
 
     def test_privilege(self) -> None:
         data: List[Dict[str, Any]] = [
-            {'role': 100}, {'role': 200}, {'is_admin': True}
+            {'role': 100}, {'role': 200}
         ]
         for d in data:
             ret: Dict[str, Any] = {'result': 'success', 'user': d}
             with patch.object(Client, 'get_user_by_id', return_value = ret):
                 assert Client().get_user_by_id(0) == ret
                 self.assertTrue(Client().user_is_privileged(0))
+
+    def test_privilege_mod(self) -> None:
+        data: List[Dict[str, Any]] = [
+            {'role': 100}, {'role': 200}, {'role': 300}
+        ]
+        for d in data:
+            ret: Dict[str, Any] = {'result': 'success', 'user': d}
+            with patch.object(Client, 'get_user_by_id', return_value = ret):
+                assert Client().get_user_by_id(0) == ret
+                self.assertTrue(Client().user_is_privileged(0, allow_moderator=True))
